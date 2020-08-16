0334b376723a66de3d3e8eb5f0355c6d57fa0ed1
genady70 <kdu70@mail.ru>
2020-08-16 17:09:54 +0300

    16.082020

---
+++ lec6/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] arg) {
+        System.out.println("buna");
+        cl1 Cl1= new cl1();
+        cl1 cl2 =new cl1();
+        float f=15.5F;
+    System.out.println(0+true);
+    }
+
+}
---
+++ lec6/src/cl1.java
@@ -0,0 +1,2 @@
+public class cl1 {
+}
